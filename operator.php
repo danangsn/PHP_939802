f650793d3bceeacecbdfc80f5431e33f882cdec7
agprasha <agprasha@ymail.com>
2015-04-22 08:42:35 +0700

    commit pertama, Versi 1

---
+++ operator.php
@@ -0,0 +1,15 @@
+<?php
+// nama file : operator_assignment.php
+$a += 2;
+$b -= 4;
+$c *= 3;
+$d /= 2;
+$e %= 5;
+$my_str.="hello";
+echo "Operator Plus Equal : \$a += 2 -> ".$a;
+echo "<br>Operator Minus Equal : \$a += 2 -> ".$b;
+echo "<br>Operator Multiply Equal : \$a += 2 -> ".$c;
+echo "<br>Operator Divide Equal : \$a += 2 -> ".$d;
+echo "<br>Operator Modulo Equal : \$a += 2 -> ".$e;
+echo "<br>Operator Concatenate Equal : \$a += 2 -> ".$my_str;
+?>
---
+++ petik.php
@@ -0,0 +1,10 @@
+<?php
+	$penjumlahan = 2 + 4;
+	$pengurangan = 2 - 4;
+	$pembagian = 2 / 4;
+	$perkalian = 2 * 4;
+	$modulus = 5%2;
+
+
+	echo 'Menampilkan penjumlahan: 2+4= '.$penjumlahan.'<br>';
+?>
